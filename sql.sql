e4cc5cfe2c2c378d8679a52c8de2216b0e09bc1d
M-split <101506255+M-split@users.noreply.github.com>
2024-01-22 22:55:05 +0000

    updated SQL file

--- sql.sql
+++ sql.sql
@@ -1,18 +1,9 @@
 CREATE Database starterdb;
 
+CREATE TABLE IF NOT EXISTS Movie ("movie_id" BIGSERIAL NOT NULL PRIMARY KEY,"name" VARCHAR NOT NULL,"director" VARCHAR NOT NULL);
 
-
-create table if not exists Movie ("movie_id" BIGSERIAL NOT NULL PRIMARY KEY,"name" VARCHAR NOT NULL,"release_date" DATE NOT NULL);
-
-insert into Movie (name, release_date) values ('Movie 1', '2023-01-23');
-insert into Movie (name, release_date) values ('Movie 2', '2023-01-23');
-insert into Movie (name, release_date) values ('Movie 3', '2023-01-23');
-insert into Movie (name, release_date) values ('Movie 4', '1987-11-03');
-insert into Movie (name, release_date) values ('Movie 5', '2010-01-23');
-
-
-INSERT INTO BasicBlock (time_stamp, previous_hash) VALUES ('2024-01-18T12:34:56Z'::TIMESTAMPTZ, 'previousHashValue1');
-INSERT INTO BasicBlock (time_stamp, previous_hash) VALUES ('2024-01-18T13:45:00Z'::TIMESTAMPTZ, 'previousHashValue2');
-INSERT INTO BasicBlock (time_stamp, previous_hash) VALUES ('2024-01-18T12:34:56Z'::TIMESTAMPTZ,, 'previousHashValue3');
-INSERT INTO BasicBlock (time_stamp, previous_hash) VALUES ('2024-01-18T14:56:00Z'::TIMESTAMPTZ, NULL);
-INSERT INTO BasicBlock (time_stamp, previous_hash) VALUES ('2024-01-18T15:30:00Z'::TIMESTAMPTZ, 'previousHashValue4');
+INSERT INTO Movie (name, director) VALUES ('Movie 1', 'director name 1');
+INSERT INTO Movie (name, director) VALUES ('Movie 2', 'director name 2');
+INSERT INTO Movie (name, director) VALUES ('Movie 3', 'director name 3');
+INSERT INTO Movie (name, director) VALUES ('Movie 4', 'director name 4');
+INSERT INTO Movie (name, director) VALUES ('Movie 5', 'director name 5');
